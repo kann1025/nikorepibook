45becd5a7a248b73381bec339da0f0e0885979c6
Kana <kana.kaji1025@gmail.com>
2026-05-31 18:59:51 +0900

    Recipeモデル作成

--- nikorepibook/app/models.py
+++ nikorepibook/app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Recipe(models.Model):
+    title = models.CharField(max_length=100)
+    servings = models.IntegerField(default=2)
+    reference_url = models.URLField(blank=True)
+    memo = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
